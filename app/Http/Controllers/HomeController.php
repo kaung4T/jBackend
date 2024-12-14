bc7eb32f1765e27aa72d3bab40bff626d0c66051
kaung4T <mrkaungminnkhant@gmail.com>
2024-12-15 02:03:50 +0630

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
-    public function index(Request $request) {
-        return view('index');
+    public function index(Request $request)
+    {
+        return response()->json(["應用程式介面" => "應用程式介面"], 200);
     }
 }
